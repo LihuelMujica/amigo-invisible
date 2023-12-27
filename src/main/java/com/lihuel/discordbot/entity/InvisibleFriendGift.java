55c0bad32dd480505d21f62550317ec1979f8c73
LihuelMujica <lihuelmujica@gmail.com>
2023-12-27 19:35:22 -0300

    Add InvisibleFriendGift entity

---
+++ src/main/java/com/lihuel/discordbot/entity/InvisibleFriendGift.java
@@ -0,0 +1,10 @@
+package com.lihuel.discordbot.entity;
+
+import lombok.Data;
+
+@Data
+public class InvisibleFriendGift {
+    private String title;
+    private String body;
+    private String imgURL;
+}
